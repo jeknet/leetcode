24490207755f5db9125f0e6d33f530ae1480362d
jeknet <jeknet@gmail.com>
2023-07-22 12:56:38 -0400

    Adding leetcode url

--- 1108-defanging-an-ip-address/solution_1_liner.py
+++ 1108-defanging-an-ip-address/solution_1_liner.py
@@ -1,3 +1,5 @@
+# https://leetcode.com/problems/defanging-an-ip-address/submissions/1001130765/
+
 class Solution:
     def defangIPaddr(self, address: str) -> str:
         return "".join(x if x != "." else "[.]" for x in address)
