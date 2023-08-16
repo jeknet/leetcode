1f395ef1f753d0952f8a59024c79eede55a8f5c8
jeknet <jeknet@gmail.com>
2023-08-16 11:44:05 -0400

    Removing use of numbers :D

--- 0709-to-lower-case/Solution1.py
+++ 0709-to-lower-case/Solution1.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/to-lower-case/submissions/1022671276/
 class Solution:
     def toLowerCase(self, s: str) -> str:  
-        return "".join([chr(ord(x)+32) if (ord(x) >= 65 and ord(x)<= 90) else x for x in s])
+        diff = ord('a') - ord('A')
+        return "".join([chr(ord(x)+diff) if (ord(x) >= ord('A') and ord(x)<= ord('Z')) else x for x in s])
