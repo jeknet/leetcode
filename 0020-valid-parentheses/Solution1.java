2d2a9b005ec5eb63e74c2d4f8987c3938c1be823
jeknet <jeknet@gmail.com>
2023-08-31 21:35:01 -0400

    Update Solution1.java

--- 0020-valid-parentheses/Solution1.java
+++ 0020-valid-parentheses/Solution1.java
@@ -1,22 +1,24 @@
-// https://leetcode.com/problems/valid-parentheses/submissions/1037258298/
+// https://leetcode.com/problems/valid-parentheses/submissions/1037260356/
 class Solution {
     Map<String, String> elements = Map.of(")", "(", "}", "{","]","[");
 
     public boolean isValid(String s) {
         Stack<String> prevs = new Stack<String>();
 
+
         for(int i = 0; i < s.length(); i++){
             String element = Character.toString(s.charAt(i));
+
+            if(prevs.size() == 0){
+                prevs.push(element);
+                continue;
+            }
             if(elements.get(element) == null){
                 prevs.push(element);
             }else{
-                if(prevs.size() > 0){
-                    if(elements.get(element).equals(prevs.peek())){
+                 if(elements.get(element).equals(prevs.peek())){
                         prevs.pop();
-                    }else{
-                        return false;
-                    }
-                }else{ 
+                }else{
                     return false;
                 }
             }
